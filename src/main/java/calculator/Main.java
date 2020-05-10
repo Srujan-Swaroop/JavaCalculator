1ecd63cb813e4dab09f2ce7c9477b0ae40871701
Srujan-Swaroop <srujan.swaroop@iiitb.org>
2020-05-10 23:12:18 +0530

    Testing

--- src/main/java/calculator/Main.java
+++ src/main/java/calculator/Main.java
@@ -1,18 +1,32 @@
 import java.util.Scanner;
 public class Main {
 
-    public static void main(String[] args) {
-        
-        int num1, num2, sum;
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter First Number: ");
-        num1 = sc.nextInt();
-        
-        System.out.println("Enter Second Number: ");
-        num2 = sc.nextInt();
-        
-        sc.close();
-        sum = num1 + num2;
-        System.out.println("Sum of these numbers: "+sum);
-    }
+    public static void main(String[] args){
+		System.out.println("6 + 3 = " + add(6, 3));
+		System.out.println("6 - 3 = " + subtract(6,3));
+		System.out.println("6 * 3 = " + multiply(6,3));
+		System.out.println("6 / 3 = " + divide(6,3));
+	}
+
+	public static int add(int a, int b) {
+		return a + b;
+	}
+	
+	public static int subtract(int a, int b) {
+		return a - b;
+	}
+
+	public static long multiply(int a, int b) {
+		return a * b;
+	}
+
+	public static double divide(int a, int b) {
+		double result;
+		if (b == 0) {
+			throw new IllegalArgumentException("Divisor cannot divide by zero");
+		} else {
+			result = Double.valueOf(a)/Double.valueOf(b);
+		}
+		return result;
+	}
 }
--- src/test/java/calculator/AppTest.java
+++ src/test/java/calculator/AppTest.java
@@ -1,20 +1,56 @@
 package calculator;
 
-import static org.junit.Assert.assertTrue;
+import org.junit.*;
 
-import org.junit.Test;
+public class CalculatorTests {
+    
+    private Calculator calculator;
+    
+    @Before
+    public void setUp() {
+        calculator = new Calculator();
+    }
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void testAdd() {
+        int a = 15;
+        int b = 20;
+        int expectedResult = 35;
+        long result = calculator.add(a, b);
+        Assert.assertEquals(expectedResult, result);;
+    }
+    
+    @Test
+    public void testSubtract() {
+        int a = 25;
+        int b = 20;
+        int expectedResult = 5;
+        long result = calculator.subtract(a, b);
+        Assert.assertEquals(expectedResult, result);;
+    }
+    
+    @Test
+    public void testMultiply() {
+        int a = 10;
+        int b = 25;
+        long expectedResult = 250;
+        long result = calculator.multiply(a, b);
+        Assert.assertEquals(expectedResult, result);;
+    }
+    
+    @Test
+    public void testDivide() {
+        int a = 56;
+        int b = 10;
+        double expectedResult = 5.6;
+        double result = calculator.divide(a, b);
+        Assert.assertEquals(expectedResult, result,0.00005);
+    }
+    
+    @Test(expected = IllegalArgumentException.class)
+    public void testDivideByZero() {
+        int a = 15;
+        int b = 0;
+        calculator.divide(a, b);
     }
 }
